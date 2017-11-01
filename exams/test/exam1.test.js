7dd96b02e41d92cb0d6df3b85db0693c528a574b
Derepa Alexey <graf_dav2@ukr.net>
2017-11-01 23:13:19 +0200

    added a new condition to file exam1.test.js

--- exams/test/exam1.test.js
+++ exams/test/exam1.test.js
@@ -12,6 +12,7 @@ describe('Exam 1', () => {
         expect(func(2000, 3000)).toEqual([[2620 , 2924]]);
         expect(func(5000, 6000)).toEqual([[5020 , 5564]]);
         expect(func(284, 500)).toEqual([]);
+        expect(func(1, 1300)).toEqual([[220, 284], [1184, 1210]]);
 
         /* Not correct input */
          expect(func(-1, 300)).toBe(false);
